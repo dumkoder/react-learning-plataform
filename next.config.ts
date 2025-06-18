4547df6ffa504308c942fe01621d2c802b461095
Danny Urrea <danny.urrea@icloud.com>
2025-06-18 13:49:26 -0500

    Add deploy files to github pages

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,13 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/react-learning-plataform' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/react-learning-plataform/' : '',
 };
 
 export default nextConfig;
